630387770aebd153e6d14a14a23debd19239414d
seo <mygusdnd@naver.com>
2020-07-29 20:08:13 +0900

    [2020.07.29]
    substring 시발

---
+++ class/java/source/class_02/src/day10/Ex03.java
@@ -0,0 +1,77 @@
+package day10;
+
+import java.util.Arrays;
+
+/*
+ 	문제 3]
+ 	
+ 		5명의 학생의
+ 		java, oracle, html, jsp
+ 		점수를 입력 받아서
+ 		총점과 석차를 구하는 프로그램을 작성하세요
+ 		
+ 		단, 입력, 총점계산, 석차계산, 출력은 함수를 작성해서
+ 		처리하세요.
+  
+  
+  		10/50/80/90/
+  		//	indexOf(int ch, int fromIndex)
+//		ch 		: 찾을 문자
+//  fromIndex : 찾을 시작위치
+//	단, 입력, 총점계산, 석차계산, 출력은 함수를 작성해서
+//		처리하세요.
+ * 	  lastIndexOf(int ch)
+	  	뒤에서 부터 찾을 문자를 검색해서 해당 위치를 반환해준다.
+ * 
+	// 입력
+ */
+
+import javax.swing.*;
+
+
+
+public class Ex03 {
+	public Ex03(){
+		
+	}
+	
+	int[][] st = new int[5][4];
+	
+	public void setPoint() {
+		
+		for(int i = 0; i<st.length; i++) {
+			String str = JOptionPane.showInputDialog("점수를입력");
+			for(int j = 0; j<st[i].length;i++) {
+				st[i][j] = Integer.parseInt(str.split("/"));
+			}
+			
+				
+		}
+		for(int i = 0 ; i<st.length;i++) {
+			for(int j = 0; j<st[i].length;j++) {
+				System.out.println(st[i]);
+			}
+		}
+		
+		
+		
+	}
+	
+	
+	
+	
+	public static void main(String[] args) {
+		
+		
+			String str = "10/20/30/40";
+			//
+			String data[] = str.split("/");
+			
+			for(int i = 0; i<data.length;i++) {
+				System.out.println(data[i]);
+			}
+		}
+		
+	}
+
+
